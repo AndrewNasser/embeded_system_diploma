8949c1742545bee3e7e1b43a22f7b9e07cc40b59
Andrew Nasser <andrewnasser67@gmail.com>
2022-08-30 11:08:31 -0700

    uart

--- project/lab 1/app.c
+++
@@ -1,7 +0,0 @@
-#include "uart.h"
-unsigned char string_buffer[100] = "Learn-in-depth";
-
-void main(void)
-{
-    Uart_send_string(string_buffer);
-}
--- project/lab 1/uart.h
+++
@@ -1,5 +0,0 @@
-//for safety my header 
-#ifndef _UART_H_
-#define _UART_H_ 
-void Uart_send_string(unsigned char* p_tx_string);
-#endif
