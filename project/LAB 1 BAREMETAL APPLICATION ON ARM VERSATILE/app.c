0df50401173209a2eb61c3f771cce9cf1206f39f
Andrew Nasser <andrewnasser67@gmail.com>
2022-09-10 00:08:07 -0700

    unit3

--- project/LAB 1 BAREMETAL APPLICATION ON ARM VERSATILE/app.c
+++ project/LAB 1 BAREMETAL APPLICATION ON ARM VERSATILE/app.c
@@ -1,7 +1,6 @@
 #include "uart.h"
-#include "platform.h"
-unsigned char string_buffer[100] = "Learn-in-depth";
 
+unsigned char string_buffer[100] = "Learn-in-depth";
 void main(void)
 {
     Uart_Send_String(&string_buffer[0]);
